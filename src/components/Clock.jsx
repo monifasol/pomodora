9540ad3eb5c1b90f74806da2d5dc53c650b26281
Monica Saiz <moni.sm@gmail.com>
2022-03-08 00:39:07 +0100

    Adding arm with banner, buttons for reset, switch, start, and clean up functionality

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -7,8 +7,7 @@ const Clock = () => {
   const maxMinutesRest = 1;
   const maxSeconds = 10;
 
-  const [ timerRunning, setTimerRunning ] = useState(false);
-  const [ currentState, setCurrentState ] = useState('focus');
+  const [ currentState, setCurrentState ] = useState(0);
   const [ minutesLeft, setMinutesLeft ] = useState(maxMinutesFocus);
   const [ secondsLeft, setSecondsLeft ] = useState(0);
   const [ currentSlot, setCurrentSlot ] = useState({});
@@ -16,15 +15,28 @@ const Clock = () => {
 
   let intervalTimerId = 0;
 
+  // legend states
+  const timerStates = {
+    0: 'When you are ready, press start!',
+    1: 'Time to focus!',
+    2: 'Focus time paused',
+    3: 'Time to rest!',
+    4: 'Resting time paused',
+    5: 'Break of 15-30 minutes!'
+  }
+
   useEffect( () => {
-    // runTimer only if it's not in the initial state (in that case we would wait for "Start timer" to be tirggered)
-    if (timerRunning) runTimer();
+    // runTimer only if it's not in the initial state (in that case we would wait for "Start timer" to trigger "runTimer()")
+    if (currentState === 1 || currentState === 3) runTimer();
     return () => clearInterval(intervalTimerId);     // cleanup function
   }, [secondsLeft] );
 
-
   const runTimer = () => {
-    if (!timerRunning) setTimerRunning(true);
+    
+    if (currentState === 0) setCurrentState(1);
+    if (currentState === 2) setCurrentState(1);
+    if (currentState === 4) setCurrentState(3);
+
     intervalTimerId = setInterval( () => { updateTimer(); }, 1000);    // every 1 sec
   };
 
@@ -43,7 +55,6 @@ const Clock = () => {
     //console.log('END SLOT. End time should be updated and slot added to array.');
     currentSlot.endTime = new Date();
     setTimeSlots([currentSlot, ...timeSlots]);
-    //console.log('currentSlot.endTime', currentSlot.endTime);
     switchTo();
   };
 
@@ -52,17 +63,23 @@ const Clock = () => {
     // if the time for the slot is gone, we end it
     if (minutesLeft === 0 && secondsLeft === 0) endSlot();
 
-    // if time slot just started, then we create new slot
-    if (currentState === 'focus' && minutesLeft === (maxMinutesFocus - 1) && secondsLeft === maxSeconds) {
-      //console.log('Then, create new slot of focus time!');
+    // If it's initial state OR if focus time slot just started
+    // Then, create new slot of focus time!
+    if ( currentState === 0 || 
+        (currentState === 1 && minutesLeft === (maxMinutesFocus - 1) && secondsLeft === maxSeconds)
+       ) {
+      setCurrentState(1);     // 1 = focus time
       createSlot('focus');
     }
 
-    if (currentState === 'rest' && minutesLeft === (maxMinutesRest - 1) && secondsLeft === maxSeconds) {
-      //console.log('Then, create new slot of resting time!');
+    // If it's initial state OR if resting time slot just started
+    // Then, create new slot of resting time!
+    if (currentState === 3 && minutesLeft === (maxMinutesRest - 1) && secondsLeft === maxSeconds) {
+      setCurrentState(3);     // 3 = rest time
       createSlot('rest');
     }
 
+    // Set seconds left
     if (secondsLeft === 0 && minutesLeft > 0 ) setMinutesLeft(minutesLeft - 1);
     const newSecondsLeft = secondsLeft === 0 ? maxSeconds : secondsLeft - 1;
     setSecondsLeft(newSecondsLeft);
@@ -72,32 +89,45 @@ const Clock = () => {
     return time.toString().length === 1 ? `0${time}` : time;
   };
 
-  const resetTimer = (currentState) => {
-    if (currentState === 'rest') {
+  function resetTimer(newState) {
+    
+    let typeTime = null;
+
+    // We first evaluate if newState exists (which means we come from switchTo)
+    if (newState && newState === 3) typeTime = 'rest'
+    else if (newState && newState === 1) typeTime = 'focus'
+    else if (isRestingTime()) typeTime = 'rest'
+    else if (isFocusTime()) typeTime = 'focus'
+
+    if (typeTime === 'rest') {
       setMinutesLeft(maxMinutesRest);
       setSecondsLeft(0);
-    } else {
+    } else if (typeTime === 'focus') {
       setMinutesLeft(maxMinutesFocus);
       setSecondsLeft(0);
     }
   };
 
   const switchTo = () => {
-    const newState = currentState === 'focus' ? 'rest' : 'focus';
+    const newState = isFocusTime() ? 3 : 1;
     setCurrentState(newState);
-    resetTimer(newState);     // otherwise it does not work :(
+    resetTimer(newState);         
   };
 
   const stopResumeTimer = () => {
-    if (timerRunning) {
-      // stop timer
+    if (isTimeRunning()) {
+      // Time is running, so we stop timer
       clearInterval(intervalTimerId);
-      setTimerRunning(false);
-      
-    } else {
-      // resume timer
+      // Set new state
+      const newState = currentState === 1 ? 2 : 4;
+      setCurrentState(newState);
+
+    } else if (isTimePaused()) {
+      // Time was paused, so we resume timer
       updateTimer();
-      setTimerRunning(true);
+      // Set new state
+      const newState = currentState === 2 ? 1 : 3;
+      setCurrentState(newState);
     }
   };
 
@@ -105,6 +135,15 @@ const Clock = () => {
     let options = { hour: '2-digit', minute: '2-digit' };
     return time.toLocaleTimeString([], options);
   };
+
+  const isFocusTime = () => currentState === 1 || currentState === 2;
+
+  const isRestingTime = () => currentState === 3 || currentState === 4 || currentState === 5;
+
+  const isTimeRunning = () => currentState === 1 || currentState === 3;
+
+  const isTimePaused = () => currentState === 2 || currentState === 4;
+
 
   return (
     <>
@@ -129,8 +168,11 @@ const Clock = () => {
         </div>  {/* end time slots display section (LEFT in flex) */}
 
         <div className='tomato-section'>
-          <h2>{currentState} time!</h2>
-          
+
+          <div className='arm-banner'>
+              <span className='arm-banner-text'>{ timerStates[currentState] }</span>
+          </div>
+
           <div className='tomato'>
             <div className='timer'>
               <span>{ digitToDisplay(minutesLeft) }</span>
@@ -139,26 +181,41 @@ const Clock = () => {
             </div>
           </div>
 
-          <p className='time-paused'>{ !timerRunning && <span>{currentState} time paused!</span>}</p>
-
           <div className='section-buttons'>
 
-            <div className='btn' onClick={runTimer}>
-              Start!
-            </div>
+            { currentState === 0 ? 
+              <div className='btn' onClick={runTimer}>
+                <span>Start!</span>
+              </div>
+              :
+              <div className='btn' onClick={stopResumeTimer}>
+                <span> 
+                  { isTimeRunning() && 'Pause timer' }
+                  { isTimePaused() && 'Resume timer' }
+                </span>
+              </div>
+            }
 
-            <div className='btn' onClick={switchTo}>
-              Swith to { currentState === 'focus' ? 'rest' : 'focus' }
-            </div>
+            { currentState !== 0 && 
+              <>
+                <div className='btn' onClick={resetTimer}>
+                  <span> 
+                    Reset
+                    { isFocusTime() && ' focus time' }
+                    { isRestingTime() && ' resting time' }
+                  </span>
+                </div>
 
-            <div className='btn' onClick={resetTimer}>
-              Reset { currentState } time
-            </div>
+                <div className='btn' onClick={switchTo}>
+                  <span>
+                    Switch to 
+                    { isFocusTime() && ' resting time' }
+                    { isRestingTime() && ' focus time' }
+                  </span>
+                </div>
+              </>
+            }
 
-            <div className='btn' onClick={stopResumeTimer}>
-              { !timerRunning ? 'Resume' : 'Pause' }
-              { ` ${currentState}` } time
-            </div>
           </div>
         </div> {/* end tomato section (RIGHT in flex) */}
       </div>
